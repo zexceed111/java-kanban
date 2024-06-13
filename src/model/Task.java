e0a7a3a66102749adca5d77bbc48478b18fea24b
Kazurov Kostya <kazurovzxc@mail.ru>
2024-06-13 16:17:06 +0200

    переопределил equals и hashCode с помощью alt + insert

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,16 +1,31 @@
 package model;
 
+import java.util.Objects;
+
 public class Task {
     private String name;
     private String description;
     private int id;
     private Status status;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id && Objects.equals(name, task.name) && Objects.equals(description, task.description) && status == task.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, id, status);
+    }
+
     public Task(String name, String description, int id) {
         this.name = name;
         this.description = description;
         this.id = id;
-        this.status = Status.NEW; // Default status of a new task
+        this.status = Status.NEW;
     }
 
 
