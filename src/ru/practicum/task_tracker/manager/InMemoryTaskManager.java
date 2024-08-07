225740ed7a89b37bccad2c2643d11b99004cf859
Kazurov Kostya <kazurovzxc@mail.ru>
2024-08-07 15:49:06 +0200

    .

--- src/ru/practicum/task_tracker/manager/InMemoryTaskManager.java
+++ src/ru/practicum/task_tracker/manager/InMemoryTaskManager.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 public class InMemoryTaskManager implements TaskManager {
 
-    private  HistoryManager historyManager = Managers.getDefaultHistory();
+    private final HistoryManager historyManager = Managers.getDefaultHistory();
 
     private final Map<Integer, Task> tasks = new HashMap<>();
     private final Map<Integer, Epic> epics = new HashMap<>();
