09c10cd533c0966767524ba48733f165af772fbc
Kazurov Kostya <kazurovzxc@mail.ru>
2024-08-11 11:22:05 +0200

    ,

--- src/ru/practicum/task_tracker/Main.java
+++ src/ru/practicum/task_tracker/Main.java
@@ -1,6 +1,5 @@
 package ru.practicum.task_tracker;
 
-import ru.practicum.task_tracker.manager.HistoryManager;
 import ru.practicum.task_tracker.manager.TaskManager;
 import ru.practicum.task_tracker.task.Epic;
 import ru.practicum.task_tracker.task.Status;
@@ -21,7 +20,7 @@ public class Main {
 
     private static void testTasks() {
         TaskManager taskManager = Managers.getDefault();
-        HistoryManager historyManager = Managers.getDefaultHistory();
+
 
         System.out.println("Тест 1: Пустой список");
         List<Task> tasks = taskManager.getTasks();
