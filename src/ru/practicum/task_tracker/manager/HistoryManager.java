732d709eb2fbd9f10f1a91c9b18c1a2709446761
Kazurov Kostya <kazurovzxc@mail.ru>
2024-08-11 11:17:51 +0200

    ,

--- src/ru/practicum/task_tracker/manager/HistoryManager.java
+++ src/ru/practicum/task_tracker/manager/HistoryManager.java
@@ -1,11 +1,13 @@
 package ru.practicum.task_tracker.manager;
 
 import ru.practicum.task_tracker.task.Task;
+
 import java.util.List;
 
 public interface HistoryManager {
 
     void add(Task task);
+
     void remove(int id);
 
     List<Task> getHistory();
