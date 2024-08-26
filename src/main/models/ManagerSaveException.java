353400422a6878e5ebbb9a48a873fc33d106cba1
Kazurov Kostya <kazurovzxc@mail.ru>
2024-08-26 15:15:16 +0200

    выровнил по стилю

--- src/main/models/ManagerSaveException.java
+++ src/main/models/ManagerSaveException.java
@@ -4,4 +4,5 @@ public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(final String message, final Throwable cause) {
         super(message, cause);
     }
+
 }
