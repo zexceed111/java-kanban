79164066aeb46f9727a094d1f3944e007d09f90e
Kazurov Kostya <kazurovzxc@mail.ru>
2024-08-26 14:46:53 +0200

    extends RuntimeException

--- src/main/models/ManagerSaveException.java
+++ src/main/models/ManagerSaveException.java
@@ -2,7 +2,7 @@ package main.models;
 
 import java.io.IOException;
 
-public class ManagerSaveException extends IOException {
+public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(final String message, final Throwable cause) {
         super(message, cause);
     }
