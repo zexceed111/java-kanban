2bed6fab94354692d98fe8bde17443756aba5dea
Kazurov Kostya <kazurovzxc@mail.ru>
2024-09-29 02:24:17 +0200

    Правка CheckStyle

--- src/main/httphandlers/TaskTest.java
+++ src/main/httphandlers/TaskTest.java
@@ -5,18 +5,14 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.List;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TaskTest {
 
     private HttpRequest createPostRequest(String uri, String jsonBody) {
-        return HttpRequest.newBuilder()
-                .uri(URI.create(uri))
-                .header("Content-Type", "application/json;charset=UTF-8")
-                .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
-                .build();
+        return HttpRequest.newBuilder().uri(URI.create(uri)).header("Content-Type", "application/json;charset=UTF-8").POST(HttpRequest.BodyPublishers.ofString(jsonBody)).build();
     }
 
     private HttpResponse<String> sendRequest(HttpRequest request) throws IOException, InterruptedException {
