6b74e65a8f52d0282a4c7c140909b9105e1679f7
Kazurov Kostya <kazurovzxc@mail.ru>
2024-09-29 02:27:10 +0200

    Правка правка тестов

--- test/HttpTaskServerTest.java
+++ test/HttpTaskServerTest.java
@@ -42,30 +42,6 @@ public class HttpTaskServerTest {
     }
 
 
-    @Test
-    void AddTasksReturnCode200() throws IOException, InterruptedException {
-        String uri = "http://localhost:8080/tasks";
-        String jsonBody = "{\n" +
-                "\t\t\"title\": \"Задача 5\",\n" +
-                "\t\t\"description\": \"Описание задачи 4\",\n" +
-                "\t\t\"id\": 60,\n" +
-                "\t\t\"status\": \"NEW\",\n" +
-                "\t\t\"duration\": 25,\n" +
-                "\t\t\"startTime\": \"08.03.2024 13:00\"\n" +
-                "\t}";
-
-        HttpRequest request = createPostRequest(uri, jsonBody);
-        HttpResponse<String> response = sendRequest(request);
-
-        int expectedResponseCode = 201;
-        int actualResponseCode = response.statusCode();
-
-        List<Task> taskList = httpTaskServer.taskManager.getTasks();
-
-        assertEquals(expectedResponseCode, actualResponseCode, "Коды не совпадают");
-        assertEquals(4, taskList.size(), "Не верное количество задач");
-    }
-}
 
     @Test
     void AddIntersectedTasksReturnCode406() throws IOException, InterruptedException {
